2d6818475484cd1b34aaf27c09b4c569972e6b44
Oktav <putrawinlyoctavianto@gmail.com>
2024-08-17 22:03:18 +0700

    f

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="text-center">
-        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! it's Sailesh Rijal </h2>
+        <h2 class="text-center text-xl sm:text-2xl font-bold uppercase dark:text-white">Hi! it's Putra Winly Octavianto </h2>
         <p class="sm:text-center text-justify my-4 sm:w-4/12 mx-auto dark:text-gray-300">
             Passionate about software development, I'm expanding my knowledge by mastering
             .Net technology. With a keen
             eye for detail and a commitment to excellence, I create captivating and user-friendly web applications. Let's
             build something amazing together!</p>
-        <p class="text-center text-blue-600">#DotNetCore #WebApplication</p>
+        <p class="text-center text-blue-600">#Vue #WebApplication</p>
     </div>
 </template>
 <script setup>
